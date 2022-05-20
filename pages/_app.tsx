3be205fd357115f09719882bddf3550c19bcf2c6
Dmytro <dmytro.osiychuk@gmail.com>
2022-05-20 21:34:03 +0200

    Add sql schema

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('sdfjhS')
   return <Component {...pageProps} />;
 }
 
